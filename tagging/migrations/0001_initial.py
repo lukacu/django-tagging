fba98b3d8fa62df6c7ec835b551fea34651c9080
Aidas Bendoraitis <aidasbend@yahoo.com>
2011-12-13 17:02:32 +0100

    * initial south migration

---
+++ tagging/migrations/0001_initial.py
@@ -0,0 +1,68 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+from base_libs.utils.misc import south_clean_multilingual_fields
+from base_libs.utils.misc import south_cleaned_fields
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Adding model 'Tag'
+        db.create_table('tagging_tag', south_cleaned_fields((
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50, db_index=True)),
+        )))
+        db.send_create_signal('tagging', ['Tag'])
+
+        # Adding model 'TaggedItem'
+        db.create_table('tagging_taggeditem', south_cleaned_fields((
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name='items', to=orm['tagging.Tag'])),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+        )))
+        db.send_create_signal('tagging', ['TaggedItem'])
+
+        # Adding unique constraint on 'TaggedItem', fields ['tag', 'content_type', 'object_id']
+        db.create_unique('tagging_taggeditem', ['tag_id', 'content_type_id', 'object_id'])
+    
+    
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'TaggedItem', fields ['tag', 'content_type', 'object_id']
+        db.delete_unique('tagging_taggeditem', ['tag_id', 'content_type_id', 'object_id'])
+
+        # Deleting model 'Tag'
+        db.delete_table('tagging_tag')
+
+        # Deleting model 'TaggedItem'
+        db.delete_table('tagging_taggeditem')
+    
+    
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('app_label', 'name')", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'tagging.tag': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'})
+        },
+        'tagging.taggeditem': {
+            'Meta': {'unique_together': "(('tag', 'content_type', 'object_id'),)", 'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': "orm['tagging.Tag']"})
+        }
+    }
+    south_clean_multilingual_fields(models)
+    
+    complete_apps = ['tagging']
